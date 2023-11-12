04d743c98b4e49c028e35c680d9147f23a9a4206
Md Sahin Alam <122547284+sahin404@users.noreply.github.com>
2023-11-13 00:30:24 +0600

    Update SJF_Preemtive_Algotighm.cpp

--- SJF_Preemtive_Algotighm.cpp
+++ SJF_Preemtive_Algotighm.cpp
@@ -1,3 +1,20 @@
+/*
+
+This program prints only the Gantt chart of the "Shortest Job First Preemptive Algorithm" for any valid input.
+
+Input Sample:
+4 
+0 8 
+1 4 
+2 9 
+3 5
+
+Output: 
+0 p1 1 p2 2 p2 3 p2 5 p4 10 p1 17 p3 26
+
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1e5;
